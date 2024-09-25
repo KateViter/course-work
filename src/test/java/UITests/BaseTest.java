3869dbfc32c9a313d6fbdf3672c055219e8996ba
pc-629 <schedrovaekaterina@gmail.com>
2024-09-25 14:46:58 +0300

    UI tests setup 3 (firefox brows)

--- src/test/java/UITests/BaseTest.java
+++ src/test/java/UITests/BaseTest.java
@@ -3,6 +3,7 @@ package UITests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -13,12 +14,13 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
 
-        ChromeOptions opt = new ChromeOptions();
-        opt.addArguments("--remote-allow-origins=*");
-        opt.addArguments("--headless");  // Add this line
-        opt.addArguments("--disable-gpu"); // Disable GPU if not needed
-        opt.addArguments("--no-sandbox"); // Required for Docker
-        driver =  new ChromeDriver(opt);
+//        ChromeOptions opt = new ChromeOptions();
+//        opt.addArguments("--remote-allow-origins=*");
+//        opt.addArguments("--headless");  // Add this line
+//        opt.addArguments("--disable-gpu"); // Disable GPU if not needed
+//        opt.addArguments("--no-sandbox"); // Required for Docker
+//        driver =  new ChromeDriver(opt);
+        driver = new FirefoxDriver();
         driver.manage().window().maximize();
 
     }
