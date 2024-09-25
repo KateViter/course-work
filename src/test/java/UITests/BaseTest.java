4230dc9939439a8337df55a8cb652404cac5462d
pc-629 <schedrovaekaterina@gmail.com>
2024-09-25 18:07:28 +0300

    UI tests setup - remoteWebDriver update 3

--- src/test/java/UITests/BaseTest.java
+++ src/test/java/UITests/BaseTest.java
@@ -14,28 +14,20 @@ public class BaseTest {
 
     protected RemoteWebDriver driver;
 
-    protected URL startStandaloneGrid() {
+    protected URL remoteUrl() {
         try {
-//            URL gridUrl = new URL("http://localhost/login:4444");
-            return new URL("http://127.0.0.1:" + "4444");
+            return new URL("http://localhost:4444");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-//      ChromeOptions opt = new ChromeOptions();
-
-//        opt.addArguments("--remote-allow-origins=*");
-//        opt.addArguments("--headless");  // Add this line
-//        opt.addArguments("--disable-gpu"); // Disable GPU if not needed
-//        opt.addArguments("--no-sandbox"); // Required for Docker
-
-//      driver =  new ChromeDriver(opt);
-//        driver =  new FirefoxDriver(opt);
 
     @BeforeMethod
     public void setUp() {
         FirefoxOptions opt = new FirefoxOptions();
-        driver = new RemoteWebDriver(startStandaloneGrid(), opt);
+        opt.addArguments("--no-sandbox");
+        opt.addArguments("--disable-dev-shm-usage");
+        driver = new RemoteWebDriver(remoteUrl(), opt);
         driver.manage().window().maximize();
     }
 
