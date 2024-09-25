fe7aa39a53c7c207b0e958bc1cc00e594522b599
pc-629 <schedrovaekaterina@gmail.com>
2024-09-25 17:15:26 +0300

    UI tests setup - remoteWebDriver update

--- src/test/java/UITests/BaseTest.java
+++ src/test/java/UITests/BaseTest.java
@@ -1,33 +1,42 @@
 package UITests;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.net.URL;
+
 public class BaseTest {
-    protected WebDriver driver;
+
+    protected RemoteWebDriver driver;
+
+    protected URL startStandaloneGrid() {
+        try {
+//            URL gridUrl = new URL("http://localhost/login:4444");
+            return new URL("http://localhost:" + "4444");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+//      ChromeOptions opt = new ChromeOptions();
+
+//        opt.addArguments("--remote-allow-origins=*");
+//        opt.addArguments("--headless");  // Add this line
+//        opt.addArguments("--disable-gpu"); // Disable GPU if not needed
+//        opt.addArguments("--no-sandbox"); // Required for Docker
+
+//      driver =  new ChromeDriver(opt);
+//        driver =  new FirefoxDriver(opt);
 
     @BeforeMethod
     public void setUp() {
-
         FirefoxOptions opt = new FirefoxOptions();
-//      ChromeOptions opt = new ChromeOptions();
-
-        opt.addArguments("--remote-allow-origins=*");
-        opt.addArguments("--headless");  // Add this line
-        opt.addArguments("--disable-gpu"); // Disable GPU if not needed
-        opt.addArguments("--no-sandbox"); // Required for Docker
-
-//      driver =  new ChromeDriver(opt);
-
-        driver = new FirefoxDriver();
+        driver = new RemoteWebDriver(startStandaloneGrid(), opt);
         driver.manage().window().maximize();
-
     }
 
     @AfterMethod
--- src/test/java/UITests/UITests.java
+++ src/test/java/UITests/UITests.java
@@ -1,13 +1,18 @@
 package UITests;
 
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
 
 public class UITests extends BaseTest{
 
 
-
     @Test
     public void start() {
-        driver.get("http://192.168.31.253/login:4444");
+        driver.get("http://192.168.31.253/login");
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys("admin");
     }
 }
